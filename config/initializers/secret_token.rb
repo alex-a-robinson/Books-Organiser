245bec16e29025bcd2dd7e01decd881a64d8c515
Alex Robinson <alex@begly.co.uk>
2014-02-26 22:58:24 +0000

    Renamed + added new posts

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '8ffb8780cc070d08536cc69631ff2ec83250e16ca94f4656707facb279c5b4f9b1ebb870001ae869b2d95d9b1d28df98f45b553d8dedefc8beafeb702de559a7'
